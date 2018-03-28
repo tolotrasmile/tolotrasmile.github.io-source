8db6e50aa05ad17c0f45931d267bed838ac05fe7
TolotraSmile <tolotra.raharison@gmail.com>
2018-03-28 17:13:16 +0300

    change message

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './assets/main.css'
 import registerServiceWorker from './registerServiceWorker'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import ApolloProvider from 'react-apollo/ApolloProvider'
+import gql from 'graphql-tag'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'https://api.github.com/graphql' }),
+  cache: new InMemoryCache()
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+  , document.getElementById('root'))
 registerServiceWorker()
